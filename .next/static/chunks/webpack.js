be58f2a3fced46bb792c8d983340f663d0b4b81e
Paulohvk <phklaine@gmail.com>
2021-12-15 13:38:18 -0300

    added node modules and changed server.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "57e50996660c7e998d4f";
+/******/ 	var hotCurrentHash = "14b2597c321feaa38511";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
