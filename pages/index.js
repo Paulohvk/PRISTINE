5f8e5423327f0b409e2861c651fa8afaeb025004
Paulohvk <phklaine@gmail.com>
2022-02-01 13:08:10 +0000

    updated homepage

--- pages/index.js
+++ pages/index.js
@@ -1,139 +1,99 @@
-import React, { Component } from 'react';
-import {Form, Button, Input, Message, Icon} from 'semantic-ui-react';
-import contract from '../Ethereum/contract0';
-import web3 from '../Ethereum/web3';
-import Common from 'ethereumjs-common';
-import { bufferToHex, privateToAddress } from 'ethereumjs-util'
-var Tx = require('ethereumjs-tx');
+import React, { Component } from "react";
+import { Button, Card, Grid, Segment } from "semantic-ui-react";
+import Layout from "../components/Layout";
+import { Link } from "../routes";
+import getEvents from "../components/getEvents";
+import getTags from "../components/getTags";
+import decodeData from "../components/decodeData";
 
 class TestIndex extends Component {
-    state = {
-        account: '',
-        errorMessage: '',
-        loading: false,
-        iconStatus: false,
-        iconMessage: 'Disconnected',
-        iconColour: 'red'
-    }
+  static async getInitialProps() {
+    var results = await getEvents();
+    const blockNumber = results.blockNumber;
+    const decodedEvents = results.events;
+    // console.log(decodedEvents);
 
-    // static async getInitialProps() {
-    //     const accounts = await web3.eth.getAccounts();
-    //     console.log(accounts[0]);
+    return { blockNumber, decodedEvents };
+  }
 
-    //     return {accounts};
-    // }
+  renderMarketPlace() {
 
-    // onClick = async(event) => {
-    //     const account = this.props.accounts[0];
-    //     console.log(account);
-    //     await web3.eth.sendTransaction({from: account,
-    //         to: "0x1206D8C3c73e40c8AfD0D878bbaa28609F9A72a9",
-    //         value: web3.utils.toWei("50", 'ether')})
-    //         .on('receipt', console.log);
-    // }
+    const style = {
+      color: "#33364d",
+    };
 
-    onClick2 = async(event) => {
-        const text = await contract.methods.text().call();
-        window.alert(text);
-    }
+    const items = this.props.decodedEvents.map((item) => {
+      // console.log(item);
+      var dataType = getTags(item);
+      const decodedData = decodeData(item);
+      const timestamp = new Date(parseInt(decodedData[1])).toLocaleString();
+      const cost = parseInt(decodedData[2]._hex,16);
 
-    // onClick3 = async(event) => {
-    //     const account = this.props.accounts[0];
-    //     console.log(account);
-    //     await contract.methods.setText("Test 1234")
-    //     .send({from: account, gas: '100000'});
-    // }
+      return {
+        header: item.transactionHash,
+        meta: "Block: " + item.blockNumber,
+        description: (
+          <Grid verticalAlign="middle">
+            <Grid.Row>
+              <Grid.Column floated="left" width={13}>
+                <div style={style}>From: 0x{item.topics[1].substr(26, 64).toUpperCase()}</div>
+                <div style={style}>Tags: {dataType}</div>
+                <div style={style}>ID: {parseInt(item.topics[2], 16)}</div>
+                <div style={style}>Cost: {cost} Tokens</div>
+                <div style={style}>Timestamp: {timestamp}</div>
+              </Grid.Column>
+              <Grid.Column floated="right" width={3}>
+                <Link route={`/data/${item.transactionHash}`}>
+                  <a>
+                    <Button content="Buy this data" icon="money" primary />
+                  </a>
+                </Link>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        ),
+        fluid: true,
+        color: 'yellow',
+      };
+    });
 
-    // onClick4 = async(event) => {
-    //     const customCommon = Common.forCustomChain(
-    //         'mainnet',
-    //         {
-    //           name: 'TestNetwork',
-    //           networkId: 7777,
-    //           chainId: 7777,
-    //         },
-    //         'petersburg',
-    //       )
+    return (
+      <div>
+        <Grid verticalAlign="middle">
+          <Grid.Row>
+            <Grid.Column floated="left" width={13}>
+              <h3>Welcome to the Blockchain-powered Data Marketplace!</h3>
+              <h3>There are currently {this.props.blockNumber} blocks in the blockchain.</h3>
+              <h3>Smart contract address: {this.props.decodedEvents[0].address}.</h3>
+              <h3>Event signature: {this.props.decodedEvents[0].topics[0]}.</h3>
+              <h3>Showing a total of {this.props.decodedEvents.length} events.</h3>
+            </Grid.Column>
+            <Grid.Column floated="right" width={3}>
+              <Link route="/data/submit">
+                <a>
+                  <Button
+                    content="Submit Data"
+                    icon="download"
+                    primary
+                  />
+                </a>
+              </Link>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Card.Group items={items}/>
+      </div>
+    );
+  }
 
-    //     var privateKey = new Buffer('e8d1574304a68b5f794d33a842389350d93e49d46b6871571f9b64eaa53131ec', 'hex')
-
-    //     const addressFrom = '0x1206D8C3c73e40c8AfD0D878bbaa28609F9A72a9';
-
-    //     const addressTo = '0xdf41d535f8c4ddb21270c0e69136a02608ac09ea';
-        
-    //     var nonceNumber = web3.utils.toHex(await web3.eth.getTransactionCount(addressFrom, 'pending'));
-    //         console.log(nonceNumber);
-    //     var rawTx = {
-    //         nonce: nonceNumber,
-    //         from: addressFrom,
-    //         gasLimit: web3.utils.toHex(25000),
-    //         gasPrice: web3.utils.toHex(10e9),
-    //         to: addressTo,
-    //         value: web3.utils.toHex(web3.utils.toWei('20', 'ether')),
-    //     }
-
-    //     console.log(rawTx);
-
-    //     var tx = new Tx.Transaction(rawTx,{common: customCommon});
-
-    //     console.log(tx);
-
-    //     tx.sign(privateKey);
-
-    //     if (
-    //         tx.validate() &&
-    //         bufferToHex(tx.getSenderAddress()) === bufferToHex(privateToAddress(privateKey))
-    //       ) {
-    //         console.log('Valid signature')
-    //       } else {
-    //         console.log('Invalid signature')
-    //       }
-        
-    //     console.log("The transaction's chain id is", tx.getChainId())
-
-    //     var serializedTx = tx.serialize();
-
-    //     await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
-    //     .on('receipt', console.log);
-    // }
-
-    connectMetamask = async(event) => {
-        event.preventDefault();
-        this.setState({loading: true, errorMessage: ''});
-
-        try {
-            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });        
-            this.state.account = accounts[0];
-            this.state.iconColour = 'green';
-            this.state.iconMessage = 'Connected';
-            console.log(this.state.account);
-        } catch (err) {
-            this.setState({errorMessage: err.message});
-            window.alert(this.state.errorMessage)
-        }
-        this.setState({loading: false});
-    }
-
-    render() {
-        
-        return (<div>
-
-            <div>Hello from Git to Vercel!</div>
-            <div>Welcome to the local Blockchain Network!</div>
-            <h3>Connect with Metamask!</h3>
-            <Button loading={this.state.loading} type='submit' onClick={this.connectMetamask}>Connect</Button>
-            <Icon name='circle' color={this.state.iconColour}>{this.state.iconMessage}</Icon>
-            <h3> Make a transaction: </h3>
-            <Button type='submit' onClick={this.onClick}>Submit</Button>
-            <h3> Check Text: </h3>
-            <Button type='submit' onClick={this.onClick2}>Check Text</Button>
-            <h3> Set new text: </h3>
-            <Button type='submit' onClick={this.onClick3}>Submit</Button>
-            <h3> Send Raw transaction: </h3>
-            <Button type='submit' onClick={this.onClick4}>Submit</Button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Layout>
+        <div>
+          {this.renderMarketPlace()}
+        </div>
+      </Layout>
+    );
+  }
 }
-
 export default TestIndex;
---
+++ pages/index_old.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import {Form, Button, Input, Message, Icon} from 'semantic-ui-react';
+import contract from '../Ethereum/contract0';
+import web3 from '../Ethereum/web3';
+import Common from 'ethereumjs-common';
+import { bufferToHex, privateToAddress } from 'ethereumjs-util'
+var Tx = require('ethereumjs-tx');
+
+class TestIndex extends Component {
+    state = {
+        account: '',
+        errorMessage: '',
+        loading: false,
+        iconStatus: false,
+        iconMessage: 'Disconnected',
+        iconColour: 'red'
+    }
+
+    // static async getInitialProps() {
+    //     const accounts = await web3.eth.getAccounts();
+    //     console.log(accounts[0]);
+
+    //     return {accounts};
+    // }
+
+    // onClick = async(event) => {
+    //     const account = this.props.accounts[0];
+    //     console.log(account);
+    //     await web3.eth.sendTransaction({from: account,
+    //         to: "0x1206D8C3c73e40c8AfD0D878bbaa28609F9A72a9",
+    //         value: web3.utils.toWei("50", 'ether')})
+    //         .on('receipt', console.log);
+    // }
+
+    onClick2 = async(event) => {
+        const text = await contract.methods.text().call();
+        window.alert(text);
+    }
+
+    // onClick3 = async(event) => {
+    //     const account = this.props.accounts[0];
+    //     console.log(account);
+    //     await contract.methods.setText("Test 1234")
+    //     .send({from: account, gas: '100000'});
+    // }
+
+    // onClick4 = async(event) => {
+    //     const customCommon = Common.forCustomChain(
+    //         'mainnet',
+    //         {
+    //           name: 'TestNetwork',
+    //           networkId: 7777,
+    //           chainId: 7777,
+    //         },
+    //         'petersburg',
+    //       )
+
+    //     var privateKey = new Buffer('e8d1574304a68b5f794d33a842389350d93e49d46b6871571f9b64eaa53131ec', 'hex')
+
+    //     const addressFrom = '0x1206D8C3c73e40c8AfD0D878bbaa28609F9A72a9';
+
+    //     const addressTo = '0xdf41d535f8c4ddb21270c0e69136a02608ac09ea';
+        
+    //     var nonceNumber = web3.utils.toHex(await web3.eth.getTransactionCount(addressFrom, 'pending'));
+    //         console.log(nonceNumber);
+    //     var rawTx = {
+    //         nonce: nonceNumber,
+    //         from: addressFrom,
+    //         gasLimit: web3.utils.toHex(25000),
+    //         gasPrice: web3.utils.toHex(10e9),
+    //         to: addressTo,
+    //         value: web3.utils.toHex(web3.utils.toWei('20', 'ether')),
+    //     }
+
+    //     console.log(rawTx);
+
+    //     var tx = new Tx.Transaction(rawTx,{common: customCommon});
+
+    //     console.log(tx);
+
+    //     tx.sign(privateKey);
+
+    //     if (
+    //         tx.validate() &&
+    //         bufferToHex(tx.getSenderAddress()) === bufferToHex(privateToAddress(privateKey))
+    //       ) {
+    //         console.log('Valid signature')
+    //       } else {
+    //         console.log('Invalid signature')
+    //       }
+        
+    //     console.log("The transaction's chain id is", tx.getChainId())
+
+    //     var serializedTx = tx.serialize();
+
+    //     await web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+    //     .on('receipt', console.log);
+    // }
+
+    connectMetamask = async(event) => {
+        event.preventDefault();
+        this.setState({loading: true, errorMessage: ''});
+
+        try {
+            const accounts = await ethereum.request({ method: 'eth_requestAccounts' });        
+            this.state.account = accounts[0];
+            this.state.iconColour = 'green';
+            this.state.iconMessage = 'Connected';
+            console.log(this.state.account);
+        } catch (err) {
+            this.setState({errorMessage: err.message});
+            window.alert(this.state.errorMessage)
+        }
+        this.setState({loading: false});
+    }
+
+    render() {
+        
+        return (<div>
+
+            <div>Hello from Git to Vercel!</div>
+            <div>Welcome to the local Blockchain Network!</div>
+            <h3>Connect with Metamask!</h3>
+            <Button loading={this.state.loading} type='submit' onClick={this.connectMetamask}>Connect</Button>
+            <Icon name='circle' color={this.state.iconColour}>{this.state.iconMessage}</Icon>
+            <h3> Make a transaction: </h3>
+            <Button type='submit' onClick={this.onClick}>Submit</Button>
+            <h3> Check Text: </h3>
+            <Button type='submit' onClick={this.onClick2}>Check Text</Button>
+            <h3> Set new text: </h3>
+            <Button type='submit' onClick={this.onClick3}>Submit</Button>
+            <h3> Send Raw transaction: </h3>
+            <Button type='submit' onClick={this.onClick4}>Submit</Button>
+            </div>
+        );
+    }
+}
+
+export default TestIndex;
